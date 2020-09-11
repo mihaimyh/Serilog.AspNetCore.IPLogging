b5c399b3b14793c575764a1464a48fd05034cc79
Mihai Dumitru <mihai-alexandru.dumitru@ing.com>
2020-09-11 07:07:11 +0300

    Add project files.

---
+++ src/Serilog.AspNetCore.IPLogging/ApplicationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Serilog.AspNetCore.IPLogging.Middlewares;
+using System;
+
+namespace Serilog.AspNetCore.IPLogging
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseSerilogRequestLogging(this IApplicationBuilder app)
+        {
+            if (app == null) throw new ArgumentNullException(nameof(app));
+
+            return app.UseMiddleware<IPAddressLoggingMiddleware>();
+        }
+    }
+}
---
+++ src/Serilog.AspNetCore.IPLogging/Middlewares/IPAddressLoggingMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serilog.Context;
+using System;
+using System.Threading.Tasks;
+
+namespace Serilog.AspNetCore.IPLogging.Middlewares
+{
+    internal class IPAddressLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public IPAddressLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext is null)
+            {
+                throw new ArgumentNullException(nameof(httpContext));
+            }
+
+            if (httpContext.Request.Headers.TryGetValue("X-Forwarded-For", out var headerValue))
+            {
+                await LogToContext(httpContext, headerValue.ToString());
+            }
+            else
+            {
+                await LogToContext(httpContext, httpContext?.Connection?.RemoteIpAddress.ToString());
+            }
+        }
+
+        private async Task LogToContext(HttpContext httpContext, string content)
+        {
+            using (LogContext.PushProperty("IPAddress", content))
+            {
+                await _next(httpContext);
+            }
+        }
+    }
+}
